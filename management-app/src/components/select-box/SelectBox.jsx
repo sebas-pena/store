a956ff7f8173d06b61b2fe7e3eec4239f80e83c2
Pablo <gmlpubli00@gmail.com>
2022-06-08 20:08:01 -0300

    avance lista productos

--- management-app/src/components/select-box/SelectBox.jsx
+++ management-app/src/components/select-box/SelectBox.jsx
@@ -3,54 +3,57 @@ import { useState, useEffect } from "react"
 import { ReactComponent as ArrowIcon } from "../../assets/svg/arrow.svg"
 import "./SelectBox.css"
 const SelectBox = ({
-	placeholder = "select option",
-	options = [],
-	handleChange = () => {},
+  placeholder = "select option",
+  options = [],
+  handleChange = () => {},
 }) => {
-	const [isOpen, setIsOpen] = useState(false)
-	const [value, setValue] = useState(placeholder)
+  const [isOpen, setIsOpen] = useState(false)
+  const [value, setValue] = useState(placeholder)
 
-	useEffect(() => {
-		const handleClickOutside = () => {
-			if (isOpen) setIsOpen(false)
-		}
-		document.addEventListener("click", handleClickOutside)
-		return () => {
-			document.removeEventListener("click", handleClickOutside)
-		}
-	}, [isOpen])
+  useEffect(() => {
+    const handleClickOutside = () => {
+      if (isOpen) setIsOpen(false)
+    }
+    document.addEventListener("click", handleClickOutside)
+    return () => {
+      document.removeEventListener("click", handleClickOutside)
+    }
+  }, [isOpen])
 
-	const handleOpenBox = (e) => {
-		e.stopPropagation()
-		setIsOpen(!isOpen)
-	}
+  const handleOpenBox = (e) => {
+    e.stopPropagation()
+    setIsOpen(!isOpen)
+  }
 
-	const handleSelectOption = (option) => {
-		setValue(option)
-		handleChange(option)
-	}
+  const handleSelectOption = (option) => {
+    setValue(option)
+    handleChange(option)
+  }
 
-	return (
-		<div className="select-box__ctn">
-			<div className="select-box__selected" onClick={handleOpenBox}>
-				<span>{value}</span>
-				<ArrowIcon className="select-box__arrow" width="15" height="15" />
-			</div>
-			<div className={`select-box__options ${isOpen ? "open" : "close"}`}>
-				{options.map((option, index) => (
-					<div
-						key={index}
-						className="select-box__option"
-						onClick={() => {
-							handleSelectOption(option)
-						}}
-					>
-						{option}
-					</div>
-				))}
-			</div>
-		</div>
-	)
+  return (
+    <div className={`select-box__ctn ${isOpen ? "open" : "close"}`}>
+      <div className="select-box__selected" onClick={handleOpenBox}>
+        <span>{value}</span>
+        <ArrowIcon className="select-box__arrow" width="15" height="15" />
+      </div>
+      <ul className="select-box__options">
+        <li className="select-box__options-title">{placeholder}</li>
+        {options.map((option, index) => (
+          <li
+            key={index}
+            className={`select-box__option ${
+              option === value ? "selected" : ""
+            }`}
+            onClick={() => {
+              handleSelectOption(option)
+            }}
+          >
+            {option}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
 
 export default SelectBox
--- management-app/src/pages/ProductsPage.jsx
+++ management-app/src/pages/ProductsPage.jsx
@@ -1,20 +1,38 @@
+import { useState, useEffect } from "react"
+
 import AddButton from "../components/button/add-button/AddButton"
+import ProductList from "../components/products-list/ProductList"
 import ProductSearch from "../components/search-input/product-search/ProductSearch"
 import SelectBox from "../components/select-box/SelectBox"
 import "./ProductsPage.css"
+
 const ProductsPage = () => {
-	return (
-		<div className="product-page__ctn">
-			<div className="product-page__header">
-				<ProductSearch />
-				<SelectBox placeholder="Sort By" options={["hola", "chaukaspian"]} />
-				<div className="product-page__add-product">
-					<AddButton>New Product</AddButton>
-				</div>
-			</div>
-			<div className="product-page__body"></div>
-		</div>
-	)
+  const [sort, setSort] = useState("")
+  const [search, setSearch] = useState("")
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    console.log(sort)
+  }, [sort])
+
+  return (
+    <div className="product-page__ctn">
+      <div className="product-page__header">
+        <ProductSearch />
+        <SelectBox
+          placeholder="Sort By"
+          options={["Name", "Price", "Rating", "Stock"]}
+          handleChange={setSort}
+        />
+        <div className="product-page__add-product">
+          <AddButton>New Product</AddButton>
+        </div>
+      </div>
+      <div className="product-page__body">
+        <ProductList />
+      </div>
+    </div>
+  )
 }
 
 export default ProductsPage

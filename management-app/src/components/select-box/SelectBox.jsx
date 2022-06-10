bcc68ca3467a19a201830c8eb574a6e0fdfc3ec3
Pablo Peña <insomnionebulous@gmail.com>
2022-06-10 01:02:08 -0300

    cambio estilo

--- management-app/src/components/select-box/SelectBox.jsx
+++ management-app/src/components/select-box/SelectBox.jsx
@@ -3,57 +3,61 @@ import { useState, useEffect } from "react"
 import { ReactComponent as ArrowIcon } from "../../assets/svg/arrow.svg"
 import "./SelectBox.css"
 const SelectBox = ({
-  placeholder = "select option",
-  options = [],
-  handleChange = () => {},
+	placeholder = "select option",
+	options = [],
+	handleChange = () => {},
 }) => {
-  const [isOpen, setIsOpen] = useState(false)
-  const [value, setValue] = useState(placeholder)
+	const [isOpen, setIsOpen] = useState(false)
+	const [value, setValue] = useState(null)
 
-  useEffect(() => {
-    const handleClickOutside = () => {
-      if (isOpen) setIsOpen(false)
-    }
-    document.addEventListener("click", handleClickOutside)
-    return () => {
-      document.removeEventListener("click", handleClickOutside)
-    }
-  }, [isOpen])
+	useEffect(() => {
+		const handleClickOutside = () => {
+			if (isOpen) setIsOpen(false)
+		}
+		document.addEventListener("click", handleClickOutside)
+		return () => {
+			document.removeEventListener("click", handleClickOutside)
+		}
+	}, [isOpen])
 
-  const handleOpenBox = (e) => {
-    e.stopPropagation()
-    setIsOpen(!isOpen)
-  }
+	const handleOpenBox = (e) => {
+		e.stopPropagation()
+		setIsOpen(!isOpen)
+	}
 
-  const handleSelectOption = (option) => {
-    setValue(option)
-    handleChange(option)
-  }
+	const handleSelectOption = (option) => {
+		setValue(option)
+		handleChange(option)
+	}
 
-  return (
-    <div className={`select-box__ctn ${isOpen ? "open" : "close"}`}>
-      <div className="select-box__selected" onClick={handleOpenBox}>
-        <span>{value}</span>
-        <ArrowIcon className="select-box__arrow" width="15" height="15" />
-      </div>
-      <ul className="select-box__options">
-        <li className="select-box__options-title">{placeholder}</li>
-        {options.map((option, index) => (
-          <li
-            key={index}
-            className={`select-box__option ${
-              option === value ? "selected" : ""
-            }`}
-            onClick={() => {
-              handleSelectOption(option)
-            }}
-          >
-            {option}
-          </li>
-        ))}
-      </ul>
-    </div>
-  )
+	return (
+		<div className={`select-box__ctn ${isOpen ? "open" : "close"}`}>
+			<div
+				className={`select-box__placeholder ${value ? "defined" : "undefined"}`}
+			>
+				{placeholder}
+			</div>
+			<div className="select-box__selected" onClick={handleOpenBox}>
+				<span>{value || placeholder}</span>
+				<ArrowIcon className="select-box__arrow" width="15" height="15" />
+			</div>
+			<ul className={` select-box__options ${isOpen ? "open" : "close"}`}>
+				{options.map((option, index) => (
+					<li
+						key={index}
+						className={`select-box__option ${
+							option === value ? "selected" : ""
+						}`}
+						onClick={() => {
+							handleSelectOption(option)
+						}}
+					>
+						{option}
+					</li>
+				))}
+			</ul>
+		</div>
+	)
 }
 
 export default SelectBox
--- management-app/src/components/star-rating/StarRating.jsx
+++ management-app/src/components/star-rating/StarRating.jsx
@@ -1,33 +1,33 @@
 import Star from "./Star"
 
 const StarRating = ({
-  rating = 2.5,
-  size = 15,
-  max = 5,
-  primary = "#ffc800",
-  secondary = "#fffcad",
+	rating = 2.5,
+	size = 15,
+	max = 5,
+	primary = "#ffd006",
+	secondary = "#f5e4a6",
 }) => {
-  return (
-    <div className="star-rating__ctn">
-      {Array(Number(max))
-        .fill(1)
-        .map((_, i) => {
-          let percentage
-          if (rating - (i + 1) >= 1) percentage = 100
-          else if (rating - (i + 1) > 0) percentage = (rating - (i + 1)) * 100
-          else percentage = 0
-          return (
-            <Star
-              primary={primary}
-              size={size}
-              secondary={secondary}
-              key={i}
-              percentage={percentage}
-            />
-          )
-        })}
-    </div>
-  )
+	return (
+		<div className="star-rating__ctn">
+			{Array(Number(max))
+				.fill(1)
+				.map((_, i) => {
+					let percentage
+					if (rating - (i + 1) >= 1) percentage = 100
+					else if (rating - (i + 1) > 0) percentage = (rating - (i + 1)) * 100
+					else percentage = 0
+					return (
+						<Star
+							primary={primary}
+							size={size}
+							secondary={secondary}
+							key={i}
+							percentage={percentage}
+						/>
+					)
+				})}
+		</div>
+	)
 }
 
 export default StarRating

90fb8ac9253274106ebbc70def5d2d769b86ecfe
Pablo Peña <insomnionebulous@gmail.com>
2022-06-11 00:38:30 -0300

    avance product page

--- management-app/src/components/pagination/Pagination.jsx
+++ management-app/src/components/pagination/Pagination.jsx
@@ -1,31 +1,87 @@
+import { useState, useEffect } from "react"
 import { ReactComponent as ArrowIcon } from "../../assets/svg/arrow.svg"
 import { ReactComponent as ElipsisIcon } from "../../assets/svg/elipsis.svg"
 import "./Pagination.css"
-const Pagination = () => {
+const Pagination = ({ total = 4, handleChange }) => {
+	const [currentPage, setCurrentPage] = useState(1)
 	return (
 		<div className="pagination__ctn">
 			<div className="pagination__buttons">
-				<button className="pagination__button">
-					<ArrowIcon width="10" style={{ transform: "rotate(-90deg)" }} />
-				</button>
-				<button className="pagination__button">1</button>
-				<ElipsisIcon
-					className="pagination__elipsis"
-					width="20"
-					fill="#7f7f81"
-				/>
-				<button className="pagination__button">40</button>
-				<p className="pagination__actual">41</p>
-				<button className="pagination__button">42</button>
-				<ElipsisIcon
-					className="pagination__elipsis"
-					width="20"
-					fill="#7f7f81"
-				/>
-				<button className="pagination__button">100</button>
-				<button className="pagination__button">
-					<ArrowIcon width="10" style={{ transform: "rotate(90deg)" }} />
-				</button>
+				{currentPage > 1 && (
+					<>
+						<button
+							className="pagination__button"
+							onClick={() => {
+								setCurrentPage(currentPage - 1)
+							}}
+						>
+							<ArrowIcon width="10" style={{ transform: "rotate(-90deg)" }} />
+						</button>
+						<button
+							className="pagination__button"
+							onClick={() => {
+								setCurrentPage(1)
+							}}
+						>
+							1
+						</button>
+					</>
+				)}
+				{currentPage > 3 && total > 3 && (
+					<ElipsisIcon
+						className="pagination__elipsis"
+						width="20"
+						fill="#7f7f81"
+					/>
+				)}
+				{currentPage > 2 && (
+					<button
+						className="pagination__button"
+						onClick={() => {
+							setCurrentPage(currentPage - 1)
+						}}
+					>
+						{currentPage - 1}
+					</button>
+				)}
+				<p className="pagination__actual">{currentPage}</p>
+				{currentPage < total - 1 && (
+					<button
+						className="pagination__button"
+						onClick={() => {
+							setCurrentPage(currentPage + 1)
+						}}
+					>
+						{currentPage + 1}
+					</button>
+				)}
+				{currentPage < total - 2 && (
+					<ElipsisIcon
+						className="pagination__elipsis"
+						width="20"
+						fill="#7f7f81"
+					/>
+				)}
+				{currentPage < total && (
+					<>
+						<button
+							className="pagination__button"
+							onClick={() => {
+								setCurrentPage(total)
+							}}
+						>
+							{total}
+						</button>
+						<button
+							className="pagination__button"
+							onClick={() => {
+								setCurrentPage(currentPage + 1)
+							}}
+						>
+							<ArrowIcon width="10" style={{ transform: "rotate(90deg)" }} />
+						</button>
+					</>
+				)}
 			</div>
 		</div>
 	)

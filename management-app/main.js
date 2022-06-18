c38a0f0619ad1dfb4d48e11daa0d48ba6a205a10
Pablo <gmlpubli00@gmail.com>
2022-06-18 18:38:15 -0300

    progreso oauth

--- management-app/main.js
+++ management-app/main.js
@@ -1,38 +1,64 @@
 const path = require("path")
+require("dotenv").config({
+  path: path.join(__dirname, ".env"),
+})
+
 const { app, BrowserWindow, ipcMain } = require("electron")
 
+const createOauthWindow = () => {
+  let authWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    show: false,
+    "node-integration": false,
+    "web-security": false,
+  })
+  authWindow.on("closed", function () {
+    authWindow = null
+  })
+  authWindow.loadURL(process.env.MELI_AUTH_URL)
+  authWindow.show()
+  authWindow.webContents.on("did-navigate", function (event, newUrl) {
+    if (newUrl.includes("code=")) {
+      var code = newUrl.split("code=")[1]
+      console.log(code)
+      authWindow.close()
+    }
+  })
+}
+
 const createWindow = () => {
-	const win = new BrowserWindow({
-		width: 800,
-		height: 600,
-		webPreferences: {
-			nodeIntegration: false,
-			contextIsolation: true,
-			preload: path.join(__dirname, "preload.js"),
-		},
-	})
-	/* win.loadFile(path.join(__dirname, "build", "index.html")) */
-	win.loadURL("http://localhost:3000")
-	win.maximize()
-	ipcMain.on("closeApp", () => {
-		app.quit()
-	})
-	ipcMain.on("maximize", () => {
-		win.maximize()
-	})
-	ipcMain.on("minimize", () => {
-		win.minimize()
-	})
-	ipcMain.on("unmaximize", () => {
-		win.unmaximize()
-	})
-	ipcMain.on("startOauth", (event, provider) => {
-		console.log(provider)
-	})
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      preload: path.join(__dirname, "preload.js"),
+    },
+  })
+  /* win.loadFile(path.join(__dirname, "build", "index.html")) */
+  win.loadURL("http://localhost:3000")
+  win.maximize()
+  ipcMain.on("closeApp", () => {
+    app.quit()
+  })
+  ipcMain.on("maximize", () => {
+    win.maximize()
+  })
+  ipcMain.on("minimize", () => {
+    win.minimize()
+  })
+  ipcMain.on("unmaximize", () => {
+    win.unmaximize()
+  })
+  ipcMain.on("startOauth", (event, provider) => {
+    createOauthWindow()
+  })
 }
 
 require("electron-reload")(__dirname, {
-	electron: require(path.join(__dirname, "..", "node_modules", "electron")),
+  electron: require(path.join(__dirname, "..", "node_modules", "electron")),
 })
 
 app.whenReady().then(createWindow)

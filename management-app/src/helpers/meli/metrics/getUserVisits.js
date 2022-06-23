749744aaac77ffbda5f2e006bac1b21797f180d0
Pablo <gmlpubli00@gmail.com>
2022-06-23 14:41:12 -0300

    Rename variables

--- management-app/src/helpers/meli/metrics/getUserVisits.js
+++ management-app/src/helpers/meli/metrics/getUserVisits.js
@@ -4,14 +4,14 @@ import fetchMeli from "../../../../helpers/fetchBase"
  * @description - Get the count of visits of a user.
  * @param {string} token - User access token
  * @param {object} product - Product object
- * @param {date} dateFrom - Date from
- * @param {date} dateTo - Date to
+ * @param {date} from - Date from
+ * @param {date} to - Date to
  * @returns {object} - API response
  */
-const getUserVisits = async (accessToken, userId, dateFrom, dateTo) => {
+const getUserVisits = async (accessToken, userId, from, to) => {
   const result = (
     await fetchMeli(
-      `users/${userId}/items_visits?date_from=${dateFrom}&date_to=${dateTo}`,
+      `users/${userId}/items_visits?date_from=${from}&date_to=${to}`,
       "GET",
       accessToken
     )

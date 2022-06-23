522b61bb6d2d7e5fea9a922bdc61b60fcc896ea5
Pablo Peña <insomnionebulous@gmail.com>
2022-06-23 01:51:51 -0300

    Add function to answer questions

--- management-app/src/helpers/meli/answerQuestion.js
+++ management-app/src/helpers/meli/answerQuestion.js
@@ -2,12 +2,12 @@ import fetchMeli from "./fetchBase"
 
 /**
  * @description - This function is used to fetch the data from the meli api
+ * @param {string} token - User access token
  * @param {string} questionId - Question id
  * @param {string} answer - User answer
- * @param {string} token - User access token
  * @returns
  */
-const getMeliUser = async (questionId, answer, accessToken) => {
+const getMeliUser = async (accessToken, questionId, answer) => {
 	const user = await fetchMeli(`answers`, "POST", accessToken, {
 		question_id: questionId,
 		text: answer,

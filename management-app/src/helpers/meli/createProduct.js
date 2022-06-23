4ac5c47e118c20825335fb32d2734c537047d612
Pablo Peña <insomnionebulous@gmail.com>
2022-06-23 01:38:52 -0300

    Add function to create products

---
+++ management-app/src/helpers/meli/createProduct.js
@@ -0,0 +1,16 @@
+import fetchMeli from "./fetchBase"
+
+/**
+ * @description - This function is used to fetch the data from the meli api
+ * @param {string} token - User access token
+ * @param {object} product - Product object
+ * @returns {object} - API response
+ */
+const createProduct = async (accessToken, product) => {
+	const result = await (
+		await fetchMeli(`items`, "POST", accessToken, product)
+	).json()
+	return result
+}
+
+export default createProduct

233d7c37422596a40c4865f5c215ac6d1541aad3
Pablo Peña <insomnionebulous@gmail.com>
2022-06-23 01:04:51 -0300

    Remove initial store

--- management-app/src/store/StoreReducer.js
+++ management-app/src/store/StoreReducer.js
@@ -1,18 +1,12 @@
-export const initialStore = {
-  title: "Login",
-  token: null,
-  user: null,
-}
-
 export const StoreReducer = (state, action) => {
-  switch (action.type) {
-    case "SET_TITLE":
-      return { ...state, title: action.payload }
-    case "SET_USER":
-      return { ...state, user: action.payload }
-    case "SET_TOKEN":
-      return { ...state, token: action.payload }
-    default:
-      return state
-  }
+	switch (action.type) {
+		case "SET_TITLE":
+			return { ...state, title: action.payload }
+		case "SET_USER":
+			return { ...state, user: action.payload }
+		case "SET_TOKEN":
+			return { ...state, token: action.payload }
+		default:
+			return state
+	}
 }

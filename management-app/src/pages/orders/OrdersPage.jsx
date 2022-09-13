db5ddc758c501d9c2be3b4e6441e74754b904aef
Pablo Peña <insomnionebulous@gmail.com>
2022-09-13 19:13:30 -0300

    Add mock

--- management-app/src/pages/orders/OrdersPage.jsx
+++ management-app/src/pages/orders/OrdersPage.jsx
@@ -7,71 +7,78 @@ import { StoreContext } from "../../store/StoreProvider"
 // svgs, css and images below
 import "./OrdersPage.css"
 
+// mocks below
+import { ordersMock } from "../../mocks/orders"
+
 const OrdersPage = () => {
-  const [statusFilter, setStatusFilter] = useState("")
-  const [page, setPage] = useState(1)
-  const { dispatch } = useContext(StoreContext)
+	const [statusFilter, setStatusFilter] = useState("")
+	const [page, setPage] = useState(1)
+	const { dispatch } = useContext(StoreContext)
 
-  useEffect(() => {
-    dispatch({ type: "SET_TITLE", payload: "Orders" })
-  }, [])
+	useEffect(() => {
+		dispatch({ type: "SET_TITLE", payload: "Orders" })
+	}, [])
 
-  return (
-    <div className="orders-page__ctn">
-      <div className="orders-page__header">
-        <p className="orders-page__status-filter">Status</p>
-        <Button
-          small
-          color={statusFilter ? "light-gray" : "primary"}
-          removeHover={statusFilter === ""}
-          onClick={() => {
-            setStatusFilter("")
-          }}
-        >
-          All
-        </Button>
-        <Button
-          small
-          color={statusFilter === "pending" ? "primary" : "light-gray"}
-          removeHover={statusFilter === "pending"}
-          onClick={() => {
-            setStatusFilter("pending")
-          }}
-        >
-          Pending
-        </Button>
-        <Button
-          small
-          color={statusFilter === "completed" ? "primary" : "light-gray"}
-          removeHover={statusFilter === "completed"}
-          onClick={() => {
-            setStatusFilter("completed")
-          }}
-        >
-          Completed
-        </Button>
-        <Button
-          small
-          color={statusFilter === "cancelled" ? "primary" : "light-gray"}
-          removeHover={statusFilter === "cancelled"}
-          onClick={() => {
-            setStatusFilter("cancelled")
-          }}
-        >
-          Cancelled
-        </Button>
-        <div className="orders-page__search">
-          <Input name="Order #" type="number" hideControls />
-        </div>
-      </div>
-      <div className="orders-page__body">
-        <OrderList orders={[]} />
-        <div className="orders-page__pagination">
-          <Pagination onChange={setPage} />
-        </div>
-      </div>
-    </div>
-  )
+	return (
+		<div className="orders-page__ctn">
+			<div className="orders-page__header">
+				<p className="orders-page__status-filter">Status</p>
+				<Button
+					small
+					color={statusFilter ? "light-gray" : "primary"}
+					removeHover={statusFilter === ""}
+					onClick={() => {
+						setStatusFilter("")
+					}}
+				>
+					All
+				</Button>
+				<Button
+					small
+					color={statusFilter === "pending" ? "primary" : "light-gray"}
+					removeHover={statusFilter === "pending"}
+					onClick={() => {
+						setStatusFilter("pending")
+					}}
+				>
+					Pending
+				</Button>
+				<Button
+					small
+					color={statusFilter === "completed" ? "primary" : "light-gray"}
+					removeHover={statusFilter === "completed"}
+					onClick={() => {
+						setStatusFilter("completed")
+					}}
+				>
+					Completed
+				</Button>
+				<Button
+					small
+					color={statusFilter === "cancelled" ? "primary" : "light-gray"}
+					removeHover={statusFilter === "cancelled"}
+					onClick={() => {
+						setStatusFilter("cancelled")
+					}}
+				>
+					Cancelled
+				</Button>
+				<div className="orders-page__search">
+					<Input name="Order #" type="number" hideControls />
+				</div>
+			</div>
+			<div className="orders-page__body">
+				<OrderList
+					orders={ordersMock.filter((order) =>
+						statusFilter ? order.status == statusFilter : true
+					)}
+				/>
+				<div className="orders-page__pagination">
+					<Pagination onChange={setPage} />
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default OrdersPage

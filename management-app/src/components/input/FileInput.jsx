b8f8d2a57c10ab20909e0a88639f1c887cb11101
Pablo Peña <insomnionebulous@gmail.com>
2022-09-13 19:10:50 -0300

    Add variant

--- management-app/src/components/input/FileInput.jsx
+++ management-app/src/components/input/FileInput.jsx
@@ -1,13 +1,28 @@
 import { ReactComponent as ImagesIcon } from "../../assets/svg/images.svg"
 import "./FileInput.css"
-const FileInput = () => {
-  return (
-    <label className="file-input">
-      <ImagesIcon width="50" height="50" fill="#6799fb" />
-      <p>Drag and drop images here or click to select images</p>
-      <input type="file" name="images" />
-    </label>
-  )
-}
+const FileInput = ({ handleFiles, handleDropFiles, simple }) => {
+	let handleDragOver = (e) => {
+		e.preventDefault()
+	}
 
+	return (
+		<label
+			className={simple ? "file-input simple" : "file-input"}
+			onDrop={handleDropFiles}
+			onDragOver={handleDragOver}
+		>
+			<ImagesIcon
+				width={simple ? "20" : "50"}
+				height={simple ? "20" : "50"}
+				fill="#6799fb"
+			/>
+			<input type="file" name="images" onChange={handleFiles} />
+			{!simple && (
+				<>
+					<p>Drag and drop images here or click to select images</p>
+				</>
+			)}
+		</label>
+	)
+}
 export default FileInput

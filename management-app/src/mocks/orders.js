4f1a622a4a7953cf48258b30ff338a3accf07cb6
Pablo Peña <insomnionebulous@gmail.com>
2022-09-13 19:09:13 -0300

    Refactor

---
+++ management-app/src/mocks/orders.js
@@ -0,0 +1,32 @@
+export const ordersMock = [
+  {
+    id: 1,
+    orderId: "12345",
+    customer: "John Doe",
+    date: "2020-01-01",
+    total: "$100.00",
+    status: "pending",
+    paymentMethod: "Credit Card",
+    products: [1, 2, 3],
+  },
+  {
+    id: 2,
+    orderId: "12346",
+    customer: "Johnnn Doe",
+    date: "2020-01-01",
+    total: "$100.00",
+    status: "cancelled",
+    paymentMethod: "Cash",
+    products: [1, 2, 3],
+  },
+  {
+    id: 3,
+    orderId: "12347",
+    customer: "Johnnn Doeee",
+    date: "2020-01-01",
+    total: "$100.00",
+    status: "completed",
+    paymentMethod: "Cash",
+    products: [1, 2, 3],
+  },
+]

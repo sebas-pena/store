8518db3019e4a338cea9689b7d2d51ebc91b3547
Pablo Peña <insomnionebulous@gmail.com>
2022-10-03 13:11:20 -0300

    Add: component to show suppliers

---
+++ management-app/src/components/suppliers-list/SuppliersList.jsx
@@ -0,0 +1,25 @@
+import SupplierItem from "./supplier-item/SupplierItem"
+import "./SuppliersList.css"
+
+const SuppliersList = ({ suppliers = [] }) => {
+	return (
+		<table className="suppliers-list__ctn">
+			<thead className="suppliers-list__header">
+				<tr>
+					<th>Name</th>
+					<th>Email</th>
+					<th>Phone</th>
+					<th>Tags</th>
+					<th></th>
+				</tr>
+			</thead>
+			<tbody>
+				{suppliers.map((supplier) => (
+					<SupplierItem supplier={supplier} key={supplier.id} />
+				))}
+			</tbody>
+		</table>
+	)
+}
+
+export default SuppliersList

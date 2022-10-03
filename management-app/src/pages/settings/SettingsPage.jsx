5755b908a59fea397ef52ed014cd0850c966a85d
Pablo Peña <insomnionebulous@gmail.com>
2022-10-03 13:14:40 -0300

    Feat: change title bar when page load

--- management-app/src/pages/settings/SettingsPage.jsx
+++ management-app/src/pages/settings/SettingsPage.jsx
@@ -1,6 +1,13 @@
-import React from "react"
+import React, { useContext, useEffect } from "react"
+import { StoreContext } from "../../store/StoreProvider"
 import "./SettingsPage.css"
 const SettingsPage = () => {
+	const { dispatch } = useContext(StoreContext)
+
+	useEffect(() => {
+		dispatch({ type: "SET_TITLE", payload: "Settings" })
+	}, [])
+
 	return <div className="settings-page__ctn">AJUSTES</div>
 }
 

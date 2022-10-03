811984d1d83852a9f3e2c1cc49a122f15a6be02c
Pablo Peña <insomnionebulous@gmail.com>
2022-10-03 13:11:55 -0300

    Feat: improve mocks

--- management-app/src/mocks/orders.js
+++ management-app/src/mocks/orders.js
@@ -3,30 +3,51 @@ export const ordersMock = [
     id: 1,
     orderId: "12345",
     customer: "John Doe",
+    customerId: 2,
     date: "2020-01-01",
-    total: "$100.00",
+    total: "$90.00",
     status: "pending",
     paymentMethod: "Credit Card",
-    products: [1, 2, 3],
+    products: [
+      { id: 1, quantity: 1, name: "Product-Name", imgUrl: "https://via.placeholder.com/75x75", unitPrice: 10, currency: "$", },
+      { id: 2, quantity: 2, name: "Product-Name", imgUrl: "https://via.placeholder.com/75x75", unitPrice: 10, currency: "$", },
+      { id: 3, quantity: 3, name: "Product-Name", imgUrl: "https://via.placeholder.com/75x75", unitPrice: 10, currency: "$", }
+    ],
+    customerEmail: "algo@gmail.com",
+    customerPhone: "123456789"
   },
   {
     id: 2,
     orderId: "12346",
     customer: "Johnnn Doe",
+    customerId: 1,
     date: "2020-01-01",
-    total: "$100.00",
+    total: "$90.00",
     status: "cancelled",
     paymentMethod: "Cash",
-    products: [1, 2, 3],
+    products: [
+      { id: 1, quantity: 1, name: "Product-Name", imgUrl: "https://via.placeholder.com/75x75", unitPrice: 10, currency: "$", },
+      { id: 2, quantity: 2, name: "Product-Name", imgUrl: "https://via.placeholder.com/75x75", unitPrice: 10, currency: "$", },
+      { id: 3, quantity: 3, name: "Product-Name", imgUrl: "https://via.placeholder.com/75x75", unitPrice: 10, currency: "$", }
+    ],
+    customerEmail: "algo@gmail.com",
+    customerPhone: "123456789"
   },
   {
     id: 3,
     orderId: "12347",
     customer: "Johnnn Doeee",
+    customerId: 3,
     date: "2020-01-01",
-    total: "$100.00",
+    total: "$90.00",
     status: "completed",
     paymentMethod: "Cash",
-    products: [1, 2, 3],
+    products: [
+      { id: 1, quantity: 1, name: "Product-Name", imgUrl: "https://via.placeholder.com/75x75", unitPrice: 10, currency: "$", },
+      { id: 2, quantity: 2, name: "Product-Name", imgUrl: "https://via.placeholder.com/75x75", unitPrice: 10, currency: "$", },
+      { id: 3, quantity: 3, name: "Product-Name", imgUrl: "https://via.placeholder.com/75x75", unitPrice: 10, currency: "$", }
+    ],
+    customerEmail: "algo@gmail.com",
+    customerPhone: "123456789"
   },
 ]

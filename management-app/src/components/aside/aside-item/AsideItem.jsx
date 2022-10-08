895fba4203f66a82e025f0d97d6d130b28d9ddba
Pablo Peña <gmlpubli00@gmail.com>
2022-10-07 23:59:28 -0300

    Feat: handle the titlebar

--- management-app/src/components/aside/aside-item/AsideItem.jsx
+++ management-app/src/components/aside/aside-item/AsideItem.jsx
@@ -1,3 +1,4 @@
+import { useContext } from "react"
 import { NavLink } from "react-router-dom"
 import { ReactComponent as DashboardIcon } from "../../../assets/svg/dashboard.svg"
 import { ReactComponent as CartIcon } from "../../../assets/svg/cart.svg"
@@ -5,7 +6,9 @@ import { ReactComponent as ProductIcon } from "../../../assets/svg/product.svg"
 import { ReactComponent as StockIcon } from "../../../assets/svg/stock.svg"
 import { ReactComponent as SettingsIcon } from "../../../assets/svg/settings.svg"
 import { ReactComponent as TruckIcon } from "../../../assets/svg/truck.svg"
+import { ReactComponent as SupportIcon } from "../../../assets/svg/support-icon.svg"
 import "./AsideItem.css"
+import { StoreContext } from "../../../store/StoreProvider"
 
 const icons = {
 	dashboard: DashboardIcon,
@@ -14,13 +17,18 @@ const icons = {
 	stock: StockIcon,
 	settings: SettingsIcon,
 	truck: TruckIcon,
+	support: SupportIcon,
 }
 const AsideItem = ({ path, icon, title }) => {
 	let Icon = icons[icon]
+	const { dispatch } = useContext(StoreContext)
 
+	const handleChangePage = () => {
+		dispatch({ type: "SET_TITLE", payload: title })
+	}
 	return (
 		<div className="aside-item__ctn">
-			<NavLink to={path} className="aside-item__btn">
+			<NavLink to={path} className="aside-item__btn" onClick={handleChangePage}>
 				<Icon width="20" height="20" />
 				<span>{title}</span>
 			</NavLink>

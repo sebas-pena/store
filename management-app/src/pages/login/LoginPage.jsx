ce82e998c0981743199a9f868e95d453b419fa97
Pablo Peña <gmlpubli00@gmail.com>
2022-10-08 00:06:26 -0300

    Feat: rework styles and remove oauth buttons

--- management-app/src/pages/login/LoginPage.jsx
+++ management-app/src/pages/login/LoginPage.jsx
@@ -1,75 +1,47 @@
 import { useEffect, useState, useContext } from "react"
 import { useNavigate } from "react-router-dom"
 import { StoreContext } from "../../store/StoreProvider"
-
+import LoginBackground from "../../assets/images/login-background.png"
 // svgs, images and css below
 import AppLogo from "../../assets/images/logo.png"
-import MeliLogo from "../../assets/images/logos/meli-logo.png"
-import ButtonWithImage from "../../components/button/button-with-image/ButtonWithImage"
 import "./LoginPage.css"
+import CheckBox from "../../components/input/checkbox/CheckBox"
+import Button from "../../components/button/Button"
 
 const LoginPage = () => {
-  const { startOauth, addEventListener, removeEventListener } = window.electron
-  const navigate = useNavigate()
-  const [isLoading, setIsLoading] = useState(false)
-  const [error, setError] = useState(null)
+	const navigate = useNavigate()
+	const [isLoading, setIsLoading] = useState(false)
+	const [error, setError] = useState(null)
 
-  const { dispatch } = useContext(StoreContext)
-
-  useEffect(() => {
-    const handleOauthResponse = async (event, response) => {
-      setIsLoading(false)
-
-      if (response.error) {
-        setError(response.error)
-        return
-      }
-
-      let token = {
-        accessToken: response.access_token,
-        refreshToken: response.refresh_token,
-        expireTimestamp: +new Date(
-          new Date().getTime() + response.expires_in * 1000
-        ),
-        scope: response.scope,
-        userId: response.user_id,
-      }
-
-      localStorage.setItem("token", JSON.stringify(token))
-      dispatch({ type: "SET_TOKEN", payload: token })
-      navigate("/")
-    }
-
-    addEventListener("oauth-callback", handleOauthResponse)
-    return () => {
-      removeEventListener("oauth-callback", handleOauthResponse)
-    }
-  }, [])
-
-  const handleStartOauth = (provider) => {
-    setIsLoading(true)
-    startOauth(provider)
-  }
-
-  return (
-    <div className="login-page">
-      <div className="login-page__ctn">
-        <figure className="login-page__logo-ctn">
-          <img src={AppLogo} alt="logo" className="app logo" />
-          <figcaption>AstroShop</figcaption>
-        </figure>
-        <p className="login-page__oauth-title">Continue with</p>
-        <ButtonWithImage
-          onClick={() => {
-            handleStartOauth("meli")
-          }}
-        >
-          <img src={MeliLogo} alt="logo de mercado libre" />
-          <span>Mercado Libre</span>
-        </ButtonWithImage>
-      </div>
-    </div>
-  )
+	return (
+		<div className="login-page">
+			<ul className="login__background">
+				<li></li>
+				<li></li>
+			</ul>
+			<div className="login-page__ctn">
+				<div className="login-page__welcome-ctn">
+					<img src={LoginBackground} alt="background" />
+					<div className="login-page__welcome">
+						<figure className="login-page__logo-ctn">
+							<img src={AppLogo} alt="logo" className="app logo" />
+							<figcaption>AstroShop</figcaption>
+						</figure>
+						<h2>WELCOME BACK</h2>
+					</div>
+				</div>
+				<div className="login-page__form">
+					<h2>Login Account</h2>
+					<input className="login-form__input" placeholder="Email or ID" />
+					<input className="login-form__input" placeholder="Password" />
+					<CheckBox text="keep me siggned in" id="keep-logged" />
+					<Button width="100%" maxWidth="200px" height="35px">
+						Login
+					</Button>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default LoginPage

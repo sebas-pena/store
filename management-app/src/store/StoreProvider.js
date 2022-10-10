b0dc58b007fac52c5521f582e31e9364ec2b7029
Pablo Peña <gmlpubli00@gmail.com>
2022-10-09 21:19:34 -0300

    Feat: store settings

--- management-app/src/store/StoreProvider.js
+++ management-app/src/store/StoreProvider.js
@@ -13,7 +13,7 @@ export const StoreProvider = ({ children }) => {
 		title: "Login",
 		token,
 		user: null,
-		settings: {}
+		settings: {},
 	}
 
 	const [store, dispatch] = useReducer(StoreReducer, initialStore)

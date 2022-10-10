ee3603975f9f7394dd10d13634293027ed7aa06d
Pablo Peña <gmlpubli00@gmail.com>
2022-10-10 20:56:50 -0300

    Feat: initializing the store it retrieves the token

--- management-app/src/store/StoreProvider.js
+++ management-app/src/store/StoreProvider.js
@@ -3,11 +3,7 @@ import { StoreReducer } from "./StoreReducer"
 
 export const StoreContext = createContext()
 export const StoreProvider = ({ children }) => {
-	let token = JSON.parse(localStorage.getItem("token"))
-
-	if (token) {
-		token = token.expireTimestamp > +new Date() ? token : null
-	}
+	let token = localStorage.getItem("token") || null
 
 	const initialStore = {
 		title: "Login",

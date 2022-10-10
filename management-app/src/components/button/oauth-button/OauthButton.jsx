06055ce9bd58bff26509cc958ad9979ba452b8c3
Pablo Peña <gmlpubli00@gmail.com>
2022-10-10 20:18:32 -0300

    Feat: remove events

--- management-app/src/components/button/oauth-button/OauthButton.jsx
+++ management-app/src/components/button/oauth-button/OauthButton.jsx
@@ -1,20 +1,12 @@
 import React from "react"
 import "./OauthButton.css"
 const OauthButton = ({ primary, secondary, icon, text }) => {
-	const handleHover = (e) => {
-		console.log(e)
-	}
 	const styles = {
 		"--primary-color": primary,
 		"--secondary-color": secondary,
 	}
 	return (
-		<button
-			className="oauth-button"
-			onMouseEnter={handleHover}
-			onMouseLeave={handleHover}
-			style={styles}
-		>
+		<button className="oauth-button" style={styles}>
 			{icon}
 			<p>{text}</p>
 		</button>

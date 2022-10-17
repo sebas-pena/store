4c33b5c0ddc0a6a167f60e69526216cf12cec960
Pablo Peña <gmlpubli00@gmail.com>
2022-10-17 11:31:40 -0300

    Fix: Use onChange on checkboxes

--- management-app/src/hooks/useForm.js
+++ management-app/src/hooks/useForm.js
@@ -2,15 +2,25 @@ import { useState } from "react"
 
 export const useForm = (initialState) => {
   const [initialValues, setInitialValues] = useState(initialState)
-
   const [values, setValues] = useState(initialState)
 
   const handleChange = (e) => {
     const target = e.target
-    setValues((values) => ({
-      ...values,
-      [target.name]: target.type === "checkbox" ? target.checked : target.value
-    }))
+
+    const newValues = {
+      ...values
+    }
+
+    const { type, name } = target
+    if (type === "checkbox") {
+      newValues[name] = target.checked
+    } else if (type === "file") {
+      if (target.files && target.files[0]) newValues[name] = target.files[0]
+      else newValues[name] = initialValues[name]
+    } else {
+      newValues[name] = target.value
+    }
+    setValues(newValues)
   }
   const resetForm = () => {
     setValues(initialValues)
